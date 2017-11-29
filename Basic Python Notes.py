54fd0e94eacb6e2ae980c3585db6f306dec8172b
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2017-11-29 10:47:51 -0800

    Added functions
    11.29.17

---
+++ Basic Python Notes.py
@@ -0,0 +1,24 @@
+print("Hello World")
+# print("Welcome to class")
+
+# This is a comment.
+# I can use this to type in text
+# But it will never run.
+'''This is a multi-line comment
+I wanted to type out a whole bunch
+but this is all
+text
+'''
+
+# Variables
+a = 5
+b = 4
+str1 = "The quick brown fox jumps over the lazy dog"
+
+print(str1)
+
+print(a + b)
+print(a - 5 * b)
+print(a * b)
+print(a / b)
+print(a**b)  # A to the power of B
--- notes.py
+++ notes.py
@@ -1,1 +1,87 @@
-print("Hello world")
+# print("Hello world")
+# # This is a new line of code
+#
+# # Mr. Wiebe
+#
+# a = 4
+# b = 3
+#
+# print(3 + 5)
+# print(5 - 3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print()
+# print("Try to figure out how this works")
+# print(13 % 5)
+#
+# # the "%" sign is a modulus. It finds the remainder.
+#
+# car_name = "The Wiebe Mobile"
+# car_type = "BMW"
+# car_cylinders = 8
+# car_mpg = 5000.9
+#
+# print("I have a car called %s. It's pretty nice." % car_name)
+# print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+#
+# # Here is where we get a little fancy
+# print("What is your name?")
+# name = input(">_ ")
+# print("Hello %s." % name)
+#
+# age = input("How old are you?")
+# print("%s?! That's really old. You belong in a retirement home." % age)
+
+# Functions
+
+
+def print_hw():
+    print("Hello World")
+
+
+print_hw()
+print_hw()
+print_hw()
+
+
+def say_hi(name):  # name is a parameter
+    print("Hello %s." % name)
+    print("Enjoy your day.")
+
+
+say_hi("John")
+
+
+def print_age(name, age):
+    print("%s is %d years old." % (name, age))
+    age += 1  # age = age + 1
+    print("Next year, they will be %d" % age)
+
+
+print_age("John", 15)
+
+
+def f(x):
+    return x**3 + 4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+# If statements
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
