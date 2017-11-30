a1552b77129095f05185df7ffedb452cb4ae7963
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2017-11-30 10:46:46 -0800

    Added Loops
    11.30.17

--- notes.py
+++ notes.py
@@ -85,3 +85,37 @@ def grade_calc(percentage):
         return "D"
     else:
         return "F"
+
+
+'''Write a function called "happy_bday"
+that "sings" (prints) Happy birthday
+
+It must take one parameter called "name"
+'''
+
+
+def happy_bday(name):
+    print("Happy birthday to you" + ",")
+    print("Happy birthday to you" + ",")
+    print("Happy birthday to " + name + ",")
+    print("Happy birthday to you" + ".")
+
+happy_bday("John")
+
+
+# Loops
+
+for num in range(10):
+    print(num + 1)
+
+
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+
+
+# Random Numbers
+
+import random  # This should be on line 1
+print(random.randint(0, 100))
