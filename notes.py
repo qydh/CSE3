f1c017cd69a62370299eb0b137c4ae5c5997e381
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2018-01-19 08:47:13 -0800

    Added Lists

--- notes.py
+++ notes.py
@@ -1,3 +1,4 @@
+"""
 # print("Hello world")
 # # This is a new line of code
 #
@@ -119,3 +120,36 @@ while a <= 10:
 
 import random  # This should be on line 1
 print(random.randint(0, 100))
+"""
+
+
+# Lists
+the_count = [1, 2, 3, 4, 5]
+shopping_list = ["Noodles", "Eggrolls", "Milk", "rice", "soda", "chips"]
+
+print(shopping_list[0])
+
+print(len(shopping_list))
+
+# Going through a list
+for item in shopping_list:
+    print(item)
+
+for num in the_count:
+    print(num * 2)
+
+len(shopping_list)  # Gives me the length of the list
+range(3)  # Gives a list of the numbers 0 through 2
+range(len(shopping_list))  # A list of EVERY index in a list
+
+for num in range(len(shopping_list)):
+    item = shopping_list[num]
+    print("The item at index %d is %s" % (num, item))
+
+# Turn things into a list
+str1 = "Hello Class!"
+listOne = list(str1)
+print(listOne)
+listOne[11] = '.'
+print(listOne)
+print("".join(listOne))
